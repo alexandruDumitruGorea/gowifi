9b1473eafce11b1bf2416ee1073d21f00ac01bd5
alexandruDumitruGorea <alexandrudumitrugorea@gmail.com>
2020-02-10 12:49:48 +0000

    Api para logeo

--- public/wp/wp-content/themes/gowifi-template/functions.php
+++ public/wp/wp-content/themes/gowifi-template/functions.php
@@ -10,22 +10,27 @@ function register_api_hooks() {
     register_rest_route(
         'custom-plugin', '/login/',
         array(
-            'methods'  => 'GET',
+            'methods'  => array('GET', 'POST'),
             'callback' => 'login',
         )
     );
-
-    function login() {
-
-        $output = array(get_home_url());
-
-        // Your logic goes here.
-        return $output;
-
+    
+    function login($request){
+        $creds = array();
+        $creds['user_login'] = $request["username"];
+        $creds['user_password'] =  $request["password"];
+        $creds['remember'] = true;
+        $user = wp_signon( $creds, false );
+    
+        if (is_wp_error($user)) {
+          $user = "fuck";
+        }
+        return $user;
     }
 }
 add_action( 'rest_api_init', 'register_api_hooks' );
 
-// http://informatica.ieszaidinvergeles.org:9028/gowifi/public/wp/wp-json/custom-plugin/login?fuck=%22jaja
+// http://informatica.ieszaidinvergeles.org:9028/gowifi/public/wp/wp-json/custom-plugin/login
+// http://informatica.ieszaidinvergeles.org:9028/gowifi/public/wp/wp-json/custom-plugin/login?username=gowifiadmin&password=gowifi-WP-2020
 
 ?>
